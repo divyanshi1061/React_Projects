7d24be38fb88b15cc2d5de391e282d2bfbc4a34c
dictalk <divyanshikhandelwal755@gmail.com>
2025-08-19 16:50:04 +0530

    themeSwitcher

--- ThemeSwitcher/src/App.jsx
+++ ThemeSwitcher/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
@@ -23,7 +22,7 @@ useEffect(() => {
     <ThemeProvider value={{
       themeMode,lightTheme,darkTheme}    }>
     
-<div className="flex flex-wrap min-h-screen items-center">
+<div className="flex flex-wrap min-h-screen items-center ">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                         <ThemeBtn />
